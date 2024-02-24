54adf8c16587a22e6ec00599ba8222ee1e2b296c
evgen <evgen1973108@gmail.com>
2024-02-24 14:59:34 +0300

    add config

--- config.py
+++ config.py
@@ -3,7 +3,3 @@ import os
 ROOT_DIR = os.path.dirname(__file__)
 
 FILE = os.path.join(ROOT_DIR, "data", "hh_json.json")
-# from pathlib import Path
-#
-# BASE_DIR = Path(__file__).parent
-# FILE = Path(BASE_DIR, "data", "hh_json.json")
